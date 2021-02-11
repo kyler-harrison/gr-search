f068de8dea1aa297c6c583e10c3e1495dcc3e429
Kyler Harrison <harri828@purdue.edu>
2021-02-10 19:01:26 -0500

    shows db query working

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const mongoUtil = require("../mongoUtil.js");
-const db = mongoUtil.getDb();
+const database = require("../dbConfig.js");
 const collection = "wordVals";
 
 router.get("/", (req, res) => {
+    // access db client by doing database.get().mongoFunctionYouWant()
+    database.get().collection(collection).findOne({word_key: "call"}, (err, result) => {
+        if (err) throw err;
+        console.log(result.title_vals.TheHawklineMonster);
+    });
     res.render("index.ejs");
-});
-
-db.collection(collection).findOne({word_key: "call"}, (err, result) => {
-    if (err) throw err;
-    console.log(result.title_vals.TheHawklineMonster);
 });
 
 module.exports = router;
