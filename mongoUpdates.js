58e3adf9d211749b556dc3e1919367d9fef2b4ec
Kyler Harrison <harri828@purdue.edu>
2021-02-17 12:37:50 -0500

    functions to insert into title meta collection and update sets in word collection (the latter takes a while and ran out of memory so idk)

---
+++ mongoUpdates.js
@@ -0,0 +1,104 @@
+const fs = require("fs");
+const mongo = require("mongodb");
+
+const uri = "mongodb://localhost:" + process.env.MONGO_URI + "/";
+const mongoClient = mongo.MongoClient(uri, {useUnifiedTopology: true});
+
+// TODO change these to test time for update
+const dbName = "bookData";
+const wordCollection = "wordVals";
+const metaCollection = "titleMetadata"
+
+// insert new docs to the existing title metadata collection,
+// if a title_key that already exists is inserted (this shouldn't happen and it doesn't matter if it does) it will be inserted because the object
+// id is th unique identifier not the title key
+async function titleInsert(titleJsonArr) {
+	console.time("titleInsert");
+	try {
+		// init db stuff
+		await mongoClient.connect();
+		const database = mongoClient.db(dbName);
+		
+		// insert multiple docs
+		await database.collection(metaCollection).insertMany(titleJsonArr);
+
+	} catch (err) {
+		console.log("oh no");
+		console.log(err);
+	} finally {
+		mongoClient.close();
+		console.timeEnd("titleInsert");
+	}
+}
+
+// updates collection of words that are {word_key: word, title_vals: {title0: 0.2, ..., titleN: 0.01}}
+// if the word_key isn't found, a new doc is inserted (upsert option)
+// else if a new title given is the same as one already in title_vals (this shouldn't happen and doens't rlly matter if it does) the val is overwritten
+// else the new titles and their values are added to title_vals
+// I believe the updates run async, which is fine because there won't be multiple updates to the same word_key in each json set
+async function wordValsUpdate(wordJsonArr) {
+	console.time("wordValUpdate");
+
+	try {
+		// init db stuff
+		await mongoClient.connect();
+		const database = mongoClient.db(dbName);
+		const collection = database.collection(wordCollection);
+
+		// if the doc with word_key isn't found, this option creates it
+		const options = {upsert: true};
+
+		// init a few vars for each word_key object in the json array inputted
+		var filter;
+		var updateDoc;
+		var titleVals;
+		var titleName;
+		var titleVal;
+		var resPromiseArr = [];
+
+		// go through each word object and update
+		for (var wordObj of wordJsonArr) {
+			// query for doc to update
+			filter = {word_key: wordObj["word_key"]};
+
+			// values to update
+			titleVals = wordObj["title_vals"];
+
+			// update each title value
+			for (var title in titleVals) {
+				titleName = "title_vals." + title;
+				subObj = {};
+				subObj[titleName] = titleVals[title];
+
+				// update expression example: {$set: {title_vals.titleN: 0.3}}
+				updateDoc = {$set: subObj};
+
+				// push Promise so updates can run async
+				resPromiseArr.push(collection.updateOne(filter, updateDoc, options));
+			}
+
+			// let Promises resolve
+			await Promise.all(resPromiseArr);
+		}
+	} catch (err) {
+		console.log("oh no");
+		console.log(err);
+	} finally {
+		// disconnect 
+		mongoClient.close();
+		console.timeEnd("wordValUpdate");
+	}
+}
+
+ 
+/*
+var rawTitle = fs.readFileSync("/mnt/c/Users/kyler/pyguy/bookOracle/v6/data/title_data_json_arrays/set0_2100_4100.json");
+var titleJsonArr = JSON.parse(rawTitle);
+titleInsert(titleJsonArr, metaCollection);
+*/
+
+var rawWords = fs.readFileSync("/mnt/c/Users/kyler/pyguy/bookOracle/v6/data/master_dict_json_arrays/set0_2100_4100.json");
+var wordJsonArr = JSON.parse(rawWords);
+wordValsUpdate(wordJsonArr);
+
+
